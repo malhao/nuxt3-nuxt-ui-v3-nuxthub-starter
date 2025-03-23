d8cae0f1843b685313a4c958ebd8424b33286c3c
Alex Malhao <alex@malhao.net>
2025-03-23 22:41:11 +0000

    Hero updates. PageMarquee added.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,24 +11,49 @@ useSeoMeta({
 </script>
 
 <template>
-  <UPageHero
-    :title="page?.hero.title"
-    :description="page?.hero.description"
-    :headline="page?.hero.headline"
-    :links="page?.hero.links"
-    orientation="horizontal"
-    reverse
-  >
-    <template #top>
-      <div class="absolute rounded-full dark:bg-(--ui-primary) blur-[300px] size-60 sm:size-80 transform -translate-x-1/2 left-1/2 -translate-y-80" />
-
-      <StarsBg />
-    </template>
-
-    <img
-      :src="'images/alex-portrait.png'"
-      alt="Alex Malhao's portrait photo"
-      class="rounded-lg shadow-2xl ring ring-(--ui-border)"
+  <div>
+    <UPageHero
+      :headline="page?.hero.headline"
+      :title="page?.hero.title"
+      :description="page?.hero.description"
+      :links="page?.hero.links"
+      orientation="horizontal"
+      reverse
     >
-  </UPageHero>
+      <template #title>
+        <MDC :value="page?.hero.title" />
+      </template>
+
+      <template #description>
+        <MDC :value="page?.hero.description" />
+      </template>
+
+      <template #top>
+        <div class="absolute rounded-full dark:bg-(--ui-primary) blur-[300px] size-60 sm:size-80 transform -translate-x-1/2 left-1/2 -translate-y-80" />
+
+        <StarsBg />
+      </template>
+
+      <img
+        :src="'images/alex-portrait.png'"
+        alt="Alex Malhao's portrait photo"
+        class="rounded-lg shadow-2xl ring ring-(--ui-border)"
+      >
+    </UPageHero>
+
+    <UPageSection title="My technologies stack">
+      <UPageMarquee>
+        <UIcon name="i-simple-icons-javascript" class="size-10 shrink-0" />
+        <UIcon name="i-simple-icons-vuedotjs" class="size-10 shrink-0" />
+        <UIcon name="i-simple-icons-nuxt" class="size-10 shrink-0" />
+        <UIcon name="i-simple-icons-tailwindcss" class="size-10 shrink-0" />
+        <UIcon name="i-simple-icons-vuetify" class="size-10 shrink-0" />
+        <UIcon name="i-simple-icons-nodedotjs" class="size-10 shrink-0" />
+        <UIcon name="i-simple-icons-postgresql" class="size-10 shrink-0" />
+        <UIcon name="i-simple-icons-mongodb" class="size-10 shrink-0" />
+        <UIcon name="i-simple-icons-docker" class="size-10 shrink-0" />
+        <UIcon name="i-mdi-microsoft-azure" class="size-10 shrink-0" />
+      </UPageMarquee>
+    </UPageSection>
+  </div>
 </template>
